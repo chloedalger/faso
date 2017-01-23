a2a175aa579ee0e9cc4bcc603b1c83e60ed262ce
chloedalger <ch.dalger@gmail.com>
2017-01-23 21:33:35 +0100

    Update constants.py

--- g2/dalger-spaenle/photobooth/scripts/constants.py
+++ g2/dalger-spaenle/photobooth/scripts/constants.py
@@ -1,7 +1,9 @@
+#Mettre des effets, a terminer
+
 EFFECTS = ["None", "Warhol", "Four"]
 
-SCREEN_W = 800 ## raspi touch
-SCREEN_H = 480 ## raspi touch
+SCREEN_W = 800 
+SCREEN_H = 480 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
